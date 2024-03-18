b14bc935f10fccd158ee6bb06b8b04e30bb0c32e
Maxfield Allison <42394355+maxfield-allison@users.noreply.github.com>
2024-03-18 18:46:29 -0500

    Update ubuntu-template-create.sh
    Prettier

--- ubuntu-template-create.sh
+++ ubuntu-template-create.sh
@@ -1,67 +1,67 @@
 #!/bin/sh
 
-# Define storage variable
-STORAGE=nas
-
-# Define the URL for the Linux image you want to download and use
+# Variables
+STORAGE="nas"
 IMAGE_URL="http://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64-disk-kvm.img"
-
-# Extract the image name from the URL for use in file operations
 IMAGE_NAME=$(basename "$IMAGE_URL")
 
-# Install necessary tools
-echo "Checking tools"
-apt update -y && apt install nano wget curl libguestfs-tools -y
+# Update system and install required tools
+echo "Installing required tools..."
+apt update -y && apt install -y nano wget curl libguestfs-tools
 
-# Remove old image
-echo "Removing old image..."
+# Remove previous image if exists
+echo "Removing old image if it exists..."
 rm -fv "$IMAGE_NAME"
 
-# Check if VM 9000 exists before attempting to destroy it
+# Destroy existing VM template if it exists
+echo "Checking for existing VM template..."
 if qm status 9000 >/dev/null 2>&1; then
-    echo "Destroying existing VM 9000..."
+    echo "Destroying existing VM template..."
     qm destroy 9000 --destroy-unreferenced-disks 1 --purge 1
 else
-    echo "VM 9000 does not exist. Skipping destroy command."
+    echo "No existing VM template found. Proceeding..."
 fi
 
-# Download new image
-echo "Downloading new image..."
+# Download the Ubuntu Cloud Image
+echo "Downloading Ubuntu Cloud Image..."
 wget --inet4-only "$IMAGE_URL"
 
-# Add agent to image
-echo "Customizing image: adding qemu-guest-agent..."
+# Customize the image with qemu-guest-agent, timezone, and SSH settings
+echo "Customizing the image..."
 virt-customize -a "$IMAGE_NAME" --install qemu-guest-agent
-
-# Set timezone
-echo "Customizing image: setting timezone..."
 virt-customize -a "$IMAGE_NAME" --timezone America/Chicago
+virt-customize -a "$IMAGE_NAME" --run-command 'sed -i "s/^PasswordAuthentication.*/PasswordAuthentication yes/" /etc/ssh/sshd_config'
+virt-customize -a "$IMAGE_NAME" --run-command 'sed -i "s/^#PermitRootLogin.*/PermitRootLogin prohibit-password/" /etc/ssh/sshd_config'
 
-# Set password auth to yes
-echo "Customizing image: enabling password authentication..."
-virt-customize -a "$IMAGE_NAME" --run-command 'sed -i s/^PasswordAuthentication.*/PasswordAuthentication\ yes/ /etc/ssh/sshd_config'
+# Update and upgrade the image
+echo "Updating and upgrading the image..."
+virt-customize -a "$IMAGE_NAME" --run-command 'apt-get update && DEBIAN_FRONTEND=noninteractive apt-get upgrade -y && apt-get clean'
 
-# Allow root login with ssh-key only
-echo "Customizing image: setting root login policy..."
-virt-customize -a "$IMAGE_NAME" --run-command 'sed -i s/^#PermitRootLogin.*/PermitRootLogin\ prohibit-password/ /etc/ssh/sshd_config'
+# Cleanup the image
+echo "Cleaning up the image..."
+virt-customize -a "$IMAGE_NAME" --run-command 'rm -rf /var/lib/apt/lists/*'
 
-# Increase image size
-echo "Resizing image..."
-qemu-img resize "$IMAGE_NAME" +5G 
+# Resize the image
+echo "Resizing the image..."
+qemu-img resize "$IMAGE_NAME" +5G
 qemu-img resize "$IMAGE_NAME" +820M
 
-# Create VM
-echo "Creating VM..."
-qm create 9000 --name "ubuntu-2204-template" --memory 4096 --cores 2 --net0 virtio,bridge=vmbr0,tag=20,firewall=0 qm set 9000 --net1 virtio,bridge=vmbr1,tag=40,firewall=0 qm set 9000 --net2 virtio,bridge=vmbr1,tag=443,firewall=0 qm set 9000 --net3 virtio,bridge=vmbr5,tag=5,firewall=0 --bios ovmf --agent enabled=1 --ostype l26 --serial0 socket --vga serial0 --machine q35 --scsihw virtio-scsi-pci
+# Create the VM template
+echo "Creating VM template..."
+qm create 9000 --name "ubuntu-2204-template" --memory 4096 --cores 2 \
+    --net0 virtio,bridge=vmbr0,tag=20,firewall=1 \
+    --net1 virtio,bridge=vmbr1,tag=40,firewall=1 \
+    --net2 virtio,bridge=vmbr1,tag=443,firewall=1 \
+    --net3 virtio,bridge=vmbr5,tag=5,firewall=1 \
+    --bios ovmf --agent enabled=1 --ostype l26 --serial0 socket \
+    --vga serial0 --machine q35 --scsihw virtio-scsi-pci
 
-# Import image to VM and convert to QCOW2 during the import
+# Import the image to VM and convert to QCOW2
 echo "Importing image to VM and converting to QCOW2 format..."
 IMPORT_OUTPUT=$(qm importdisk 9000 "$IMAGE_NAME" $STORAGE --format qcow2 2>&1)
-
-# Extract the disk name from the import output, removing the 'unused0:' prefix
 DISK_NAME=$(echo "$IMPORT_OUTPUT" | grep -oP "Successfully imported disk as \'\K[^']+" | sed 's/^unused0://')
 
-# Check if the disk name was captured
+# Verify disk name was captured
 if [ -z "$DISK_NAME" ]; then
     echo "Failed to capture the disk name from the import operation."
     exit 1
@@ -69,24 +69,23 @@ else
     echo "Imported disk name: $DISK_NAME"
 fi
 
-# Adding disk to VM using the captured disk name
-echo "Disk to be added: $DISK_NAME"
+# Add the disk to VM
+echo "Adding disk to VM template..."
 qm set 9000 --scsi0 "$DISK_NAME"
 
-# Set bootdisk to the newly added disk
+# Set the boot disk
 echo "Setting boot disk..."
 qm set 9000 --boot c --bootdisk scsi0
 
-# Convert to template
+# Convert VM to template
 echo "Converting VM to template..."
 QM_TEMPLATE_OUTPUT=$(qm template 9000 2>&1)
 
-# Check for known 'chattr' errors and provide guidance
+# Check for errors and complete
 if echo "$QM_TEMPLATE_OUTPUT" | grep -q "chattr: Operation not supported"; then
-    echo "Note: The 'chattr' operation is not supported on the storage used. This is expected for certain filesystems (e.g., NFS) and does not impact the functionality of the VM template."
+    echo "Note: 'chattr' operation not supported on this storage. This does not impact the template functionality."
 else
     echo "VM template conversion completed successfully."
 fi
 
-echo "Script completed."
-
+echo "Template creation script completed."
